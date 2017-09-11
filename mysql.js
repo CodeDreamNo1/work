956d416323bf4805852664689f345e36f60a6e97
gys <245882185@qq.com>
2017-09-11 23:07:20 +0800

    work

---
+++ mysql.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Administrator on 2017/5/6.
+ */
+var mysql=require('mysql');
+module.exports=mysql.createConnection({
+    host:'sqld.duapp.com',
+    user:'28ddee562cdd4b13936a73fb68b22598',
+    password:'b1d9d503c2a046a5a555cb2875ee1838',
+    port:4050,
+    database:"aqAcDEkXKNhFBFHvPzRb"
+});
